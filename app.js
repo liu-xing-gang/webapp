6622c5accc05b4896818c5df58e777b83deeacfe
Liu Xinggang <44444536@163.com>
2018-06-01 18:06:04 +0800

    需要兼容三端，ios, andrio, h5

--- app.js
+++ app.js
@@ -6,6 +6,23 @@ function resize() {
     if(deviceWidth >= 750) deviceWidth = 750
     document.documentElement.style.fontSize = deviceWidth / 7.5 + 'px';
 }
+
+/**
+ * 判断ISO,Andrio, 网页
+ */
+function isiOSApp() {
+    var userAgentInfo = navigator.userAgent;
+    var Agents = ["ios_app"];
+    var flag = false;
+    for (var v = 0; v < Agents.length; v++) {
+        if (userAgentInfo.indexOf(Agents[v]) > 0) {
+            flag = true;
+            break;
+        }
+    }
+    return flag;
+}
+
 window.onorientationchange = window.onresize = function(){
 	resize()
 }
