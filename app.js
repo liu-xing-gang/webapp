5b3b984008f4d4b7e59a52d00abbf99755d02a2b
Liu Xinggang <44444536@163.com>
2018-06-08 18:39:42 +0800

    need to perfet it on weekend \(^o^)/ 欧耶

--- app.js
+++ app.js
@@ -1,13 +1,4 @@
 /**
- * rem适配
- */
-function resize() {
-    var deviceWidth = document.documentElement.clientWidth
-    if(deviceWidth >= 750) deviceWidth = 750
-    document.documentElement.style.fontSize = deviceWidth / 7.5 + 'px';
-}
-
-/**
  * 判断ISO,Andrio, 网页
  */
 function isiOSApp() {
@@ -23,11 +14,79 @@ function isiOSApp() {
     return flag;
 }
 
-window.onorientationchange = window.onresize = function(){
-	resize()
-}
-resize();
-
-if(isiOSApp()){
+if (isiOSApp()) {
     window.webkit.messageHandlers.nav.postMessage('hide');
+}
+
+/**
+ * 定义命名空间
+ */
+var Ccp = window.Ccp = {}
+
+
+/**
+ * 惰性加载单体
+ */
+Ccp.Common = (function () {
+    var uniqueInstance
+
+    function constructor() {
+        // 私有属性
+        var privateAttr1 = 1
+
+        // 私有方法
+        /**
+         * rem适配
+         */
+        function resize() {
+            var deviceWidth = document.documentElement.clientWidth
+            if (deviceWidth >= 750) deviceWidth = 750
+            document.documentElement.style.fontSize = deviceWidth / 7.5 + 'px';
+        }
+
+        return {
+            publicAttr1: 'public-attr1',
+            init: function () {
+                // 初始化
+                resize()
+            }
+        }
+    }
+
+    return {
+        getInstance: function () {
+            if (!uniqueInstance)
+                uniqueInstance = constructor()
+            return uniqueInstance
+        }
+    }
+
+})()
+
+
+// 调用
+// Ccp.Common.getInstance().pubMethod1()
+Ccp.Common.getInstance().init()
+window.onorientationchange = window.onresize = function () {Ccp.Common.getInstance().init()}
+
+window.onload = function(){
+    mui('#nav-bar').on('tap', 'a', function() {
+        var href = this.getAttribute('href');
+        
+        //非plus环境，直接走href跳转
+        if(!mui.os.plus) {
+            location.href = href;
+            return;
+        }
+    })
+
+    mui('.mui-table-view').on('tap', 'a', function() {
+        var href = this.getAttribute('href');
+        
+        //非plus环境，直接走href跳转
+        if(!mui.os.plus) {
+            location.href = href;
+            return;
+        }
+    })
 }
